064209e84532dcb697953aacdff7b09494f3be3f
it4e <limpands@gmail.com>
2016-12-15 18:39:04 +0100

    std_malloc/realloc function name fix

--- core/src/std.h
+++ core/src/std.h
@@ -1,7 +1,7 @@
 #ifndef STD_H_
 #define STD_H_
 
-void * std_std_malloc(size_t);
-void * std_std_realloc(void *, size_t);
+void * std_malloc(size_t);
+void * std_realloc(void *, size_t);
 
 #endif /* STD_H_ */
