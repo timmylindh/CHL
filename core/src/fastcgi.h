15325aa74996a7ff955cc1e4d51e42567f29fa96
Timmy Lindholm <timmylindh@gmail.com>
2022-03-18 17:59:26 +0100

    Cleaned for CHL 2.0

--- core/src/fastcgi.h
+++
@@ -1,7 +0,0 @@
-#ifndef FASTCGI_H_
-#define FASTCGI_H_
-
-void chl_fcgi_init();
-char chl_fcgi_next();
-
-#endif /* FASTCGI_H_ */
--- core/src/std.h
+++
@@ -1,7 +0,0 @@
-#ifndef STD_H_
-#define STD_H_
-
-void * std_malloc(size_t);
-void * std_realloc(void *, size_t);
-
-#endif /* STD_H_ */
